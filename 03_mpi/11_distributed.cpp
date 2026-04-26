abe1c0b49b3a518da55fea02885c1d8fa1486caf
tjy-dev <yj_ykt@icloud.com>
2026-04-27 03:06:28 +0900

    Rewrite distributed MPI example with MPI_Put

--- 03_mpi/11_distributed.cpp
+++ 03_mpi/11_distributed.cpp
@@ -13,22 +13,32 @@ int main(int argc, char** argv) {
   int size, rank;
   MPI_Comm_size(MPI_COMM_WORLD, &size);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  Body ibody[N/size], jbody[N/size];
+  Body ibody[N/size], jbody[N/size], buffer[N/size];
   srand48(rank);
   for(int i=0; i<N/size; i++) {
     ibody[i].x = jbody[i].x = drand48();
     ibody[i].y = jbody[i].y = drand48();
     ibody[i].m = jbody[i].m = drand48();
-    ibody[i].fx = jbody[i].fx = ibody[i].fy = jbody[i].fy = 0;
+    ibody[i].fx = jbody[i].fx = buffer[i].fx = 0;
+    ibody[i].fy = jbody[i].fy = buffer[i].fy = 0;
   }
-  int recv_from = (rank + 1) % size;
+  // int recv_from = (rank + 1) % size;
   int send_to = (rank - 1 + size) % size;
   MPI_Datatype MPI_BODY;
   MPI_Type_contiguous(5, MPI_DOUBLE, &MPI_BODY);
   MPI_Type_commit(&MPI_BODY);
+  int s = N/size;
+  MPI_Win win;
+  MPI_Win_create(jbody, s*sizeof(Body), sizeof(Body), MPI_INFO_NULL, MPI_COMM_WORLD, &win);
   for(int irank=0; irank<size; irank++) {
-    MPI_Send(jbody, N/size, MPI_BODY, send_to, 0, MPI_COMM_WORLD);
-    MPI_Recv(jbody, N/size, MPI_BODY, recv_from, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    //MPI_Send(jbody, N/size, MPI_BODY, send_to, 0, MPI_COMM_WORLD);
+    //MPI_Recv(jbody, N/size, MPI_BODY, recv_from, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    for(int i=0; i<s; i++) {
+      buffer[i] = jbody[i];
+    }
+    MPI_Win_fence(0, win);
+    MPI_Put(buffer, s, MPI_BODY, send_to, 0, s, MPI_BODY, win);
+    MPI_Win_fence(0, win);
     for(int i=0; i<N/size; i++) {
       for(int j=0; j<N/size; j++) {
         double rx = ibody[i].x - jbody[j].x;
@@ -49,5 +59,6 @@ int main(int argc, char** argv) {
       }
     }
   }
+  MPI_Win_free(&win);
   MPI_Finalize();
 }
